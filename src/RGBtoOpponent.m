2cdd9b0e7439d355b95da73e25421a7b7252af96
Anna Keune <ackeune@gmail.com>
2012-11-09 13:38:29 +0100

    changed the calculation to the opponent colour space to the one in the article (page 4), old calculation commented out

--- src/RGBtoOpponent.m
+++ src/RGBtoOpponent.m
@@ -1,5 +1,6 @@
 function output = RGBtoOpponent( RGB_image )
 % RGBtoOpponent converts a RGB image to the Opponent colour space
+    %{
     % Initialize image
     output = zeros( size(RGB_image) );
     
@@ -18,5 +19,22 @@ function output = RGBtoOpponent( RGB_image )
                     RGB_image(:,:,2) );
 
 	return;
+	%}
+	
+	%
+	% Initialize image
+    output = zeros( size(RGB_image) );
+    
+    % Calculate O1 = (R-G)/sqrt(2)
+    output(:,:,1) = ( RGB_image(:,:,1) - RGB_image(:,:,2) ) / sqrt(2);
+                
+    % Calculate O2 (R+G-2B)/sqrt(6)
+    output(:,:,2) = ( RGB_image(:,:,1) + RGB_image(:,:,2) - 2*RGB_image(:,:,3) ) / sqrt(6);
+                    
+    % Calculate O3 = (R+G+B)/sqrt(3)
+    output(:,:,3) = ( RGB_image(:,:,1) + RGB_image(:,:,2) + RGB_image(:,:,3) ) / sqrt(3);
+
+	return;
+	%
 end
 
